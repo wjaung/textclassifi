5fda40078ae54cc899dee680e911edcdaa6ec3f2
wjaung <68853521+wjaung@users.noreply.github.com>
2020-07-27 20:59:33 +0900

    Update TextClassification.py

--- TextClassification.py
+++ TextClassification.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 ########################################################################
 # Text clasification to identify nature-related pandemic articles
 ########################################################################
